2ff944e14b075b0c37a6e8c564f21be392c3d947
arreguy <brenoarreguy@gmail.com>
2023-03-22 19:48:32 -0300

    Fix logic bug in function return

--- script.js
+++ script.js
@@ -10,27 +10,29 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection == computerSelection) {
-        return "It's a draw!"
+        return "It's a draw!";
     } else if(playerSelection == "ROCK" && computerSelection == "SCISSORS" 
               || playerSelection == "SCISSORS" && computerSelection == "PAPER"
               || playerSelection == "PAPER" && computerSelection == "ROCK") {
                 playerScore++;
-                return `You Win! ${playerSelection} beats ${computerSelection}`
+                return `You Win! ${playerSelection} beats ${computerSelection}`;
             } else computerScore++;
-                   return `You Lose! ${computerSelection} beats ${playerSelection}`
+                   return `You Lose! ${computerSelection} beats ${playerSelection}`;
 }
 
 function game() {
     while(computerScore != 5 && playerScore != 5) {
-        playerSelection = prompt("Pick your choice: ")
+        playerSelection = prompt("Pick your choice: ");
         playerSelection = playerSelection.toUpperCase();
         let computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
     }
 
     if(playerScore == 5) {
-        console.log("CONGRATULATIONS! Try to defend your title!")
+        console.log("CONGRATULATIONS! Try to defend your title!");
+        return;
     } else {
-        console.log("GAME OVER! Better luck next time!")
+        console.log("GAME OVER! Better luck next time!");
+        return;
     }
 }
